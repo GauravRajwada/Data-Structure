8147d30fed3afcde03c521d932abdafb2f8a91d1
gauravrajwada <sintg1999@gmail.com>
2021-06-08 22:24:56 +0530

    8 july 2021

---
+++ String/11. Unique Morse Code Words.py
@@ -0,0 +1,37 @@
+"""
+International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows: "a" maps to ".-", "b" maps to "-...", "c" maps to "-.-.", and so on.
+For convenience, the full table for the 26 letters of the English alphabet is given below:
+[".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+Now, given a list of words, each word can be written as a concatenation of the Morse code of each letter. For example, "cab" can be written as "-.-..--...", (which is the concatenation "-.-." + ".-" + "-..."). We'll call such a concatenation, the transformation of a word.
+Return the number of different transformations among all words we have.
+
+Example:
+Input: words = ["gin", "zen", "gig", "msg"]
+Output: 2
+Explanation: 
+The transformation of each word is:
+"gin" -> "--...-."
+"zen" -> "--...-."
+"gig" -> "--...--."
+"msg" -> "--...--."
+
+There are 2 different transformations, "--...-." and "--...--.".
+"""
+class Solution:
+    def uniqueMorseRepresentations(self, words: List[str]) -> int:
+        if len(words)==0:
+            return 0
+        mos=[".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+        has={}
+        for i in range(97,123):
+            has[chr(i)]=mos[i-97]
+        s=[]
+        for i in words:
+            temp=[]
+            for j in i:
+                temp.append(has[j])
+            temp="".join(temp)
+            s.append(temp)
+        
+        s=set(s)
+        return len(s)
---
+++ String/9. To Lower Case.py
@@ -0,0 +1,18 @@
+"""
+Example 1:
+Input: s = "Hello"
+Output: "hello"
+
+Example 2:
+Input: s = "here"
+Output: "here"
+
+Example 3:
+Input: s = "LOVELY"
+Output: "lovely"
+"""
+
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+        return s.lower()
+        
